c4f7364f624841926c8e338476cb295e37b72f4d
SamiraRebholz <147277292+SamiraRebholz@users.noreply.github.com>
2024-12-07 15:55:32 +0000

    add modal, styling

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -3,10 +3,10 @@ class MyNavbar extends HTMLElement {
       this.innerHTML = `
         <nav>
           <ul>
-            <li><a href="">Vegetables</a></li>
-            <li><a href="">Fruits</a></li>
-            <li><a href="">Meat, Fish and Eggs</a></li>
-            <li><a href="">Dairy</a></li>
+            <li><a id="vegetables" href="./index.html">Vegetables</a></li>
+            <li><a id="fruits" href="">Fruits</a></li>
+            <li><a id="meat" href="">Meat, Fish and Eggs</a></li>
+            <li><a id="dairy" href="">Dairy</a></li>
           </ul>
         </nav>
       `;
